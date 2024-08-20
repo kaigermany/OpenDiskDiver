dd2dc8e0bdcce61b400d8137516f648408b25341
kaigermany <48881973+kaigermany@users.noreply.github.com>
2024-08-20 21:40:54 +0200

    separated LimitedReadableSource class

--- src/main/java/me/kaigermany/opendiskdiver/data/partition/Partition.java
+++ src/main/java/me/kaigermany/opendiskdiver/data/partition/Partition.java
@@ -1,7 +1,6 @@
 package me.kaigermany.opendiskdiver.data.partition;
 
-import java.io.IOException;
-
+import me.kaigermany.opendiskdiver.reader.LimitedReadableSource;
 import me.kaigermany.opendiskdiver.reader.ReadableSource;
 
 public class Partition {
@@ -36,27 +35,5 @@ public class Partition {
 
 	private static ReadableSource createLimitedReader(ReadableSource source, long offset, long len) {
 		return new LimitedReadableSource(source, offset, len);
-	}
-	
-	public static class LimitedReadableSource implements ReadableSource {
-		private ReadableSource source;
-		private long offset, len;
-		
-		public LimitedReadableSource(ReadableSource source, long offset, long len) {
-			this.source = source;
-			this.offset = offset;
-			this.len = len;
-		}
-
-		@Override
-		public void readSectors(long sectorNumber, int sectorCount, byte[] buffer, int bufferOffset) throws IOException {
-			if((sectorNumber + offset) * 512 + (buffer.length - bufferOffset) > (offset + len) * 512) throw new IOException("Invalid read: outside of Partition!");
-			source.readSectors(sectorNumber + offset, sectorCount, buffer, bufferOffset);
-		}
-
-		@Override
-		public long numSectors() {
-			return len;
-		}
 	}
 }
---
+++ src/main/java/me/kaigermany/opendiskdiver/reader/LimitedReadableSource.java
@@ -0,0 +1,25 @@
+package me.kaigermany.opendiskdiver.reader;
+
+import java.io.IOException;
+
+public class LimitedReadableSource implements ReadableSource {
+	private ReadableSource source;
+	private long offset, len;
+	
+	public LimitedReadableSource(ReadableSource source, long offset, long len) {
+		this.source = source;
+		this.offset = offset;
+		this.len = len;
+	}
+
+	@Override
+	public void readSectors(long sectorNumber, int sectorCount, byte[] buffer, int bufferOffset) throws IOException {
+		if((sectorNumber + offset) * 512 + (buffer.length - bufferOffset) > (offset + len) * 512) throw new IOException("Invalid read: outside of Partition!");
+		source.readSectors(sectorNumber + offset, sectorCount, buffer, bufferOffset);
+	}
+
+	@Override
+	public long numSectors() {
+		return len;
+	}
+}
