d01acaa8e6f31841badb0e20df135b9c7b45072f
kaigermany <48881973+kaigermany@users.noreply.github.com>
2024-09-06 02:15:15 +0200

    Main update after a very long time

--- src/main/java/me/kaigermany/opendiskdiver/Main.java
+++ src/main/java/me/kaigermany/opendiskdiver/Main.java
@@ -1,22 +1,18 @@
 package me.kaigermany.opendiskdiver;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
+import java.util.ArrayList;
 
-import me.kaigermany.opendiskdiver.data.DriveInfo;
 import me.kaigermany.opendiskdiver.data.Reader;
-import me.kaigermany.opendiskdiver.data.ntfs.NtfsNode;
+import me.kaigermany.opendiskdiver.data.fat.FatReader;
 import me.kaigermany.opendiskdiver.data.ntfs.NtfsReader;
 import me.kaigermany.opendiskdiver.data.partition.Partition;
 import me.kaigermany.opendiskdiver.data.partition.PartitionReader;
 import me.kaigermany.opendiskdiver.gui.Screen;
 import me.kaigermany.opendiskdiver.probe.Probe;
 import me.kaigermany.opendiskdiver.probe.ProbeFunction;
-import me.kaigermany.opendiskdiver.probe.ProbeResult;
 import me.kaigermany.opendiskdiver.reader.ReadableSource;
 import me.kaigermany.opendiskdiver.utils.ByteArrayUtils;
 import me.kaigermany.opendiskdiver.windows.SelectDriveGui;
-import me.kaigermany.opendiskdiver.windows.WindowsDrives;
 import me.kaigermany.opendiskdiver.windows.console.ConsoleInterface;
 
 public class Main {
@@ -36,62 +32,79 @@ public class Main {
 				return Signature == 0x202020205346544EL ? 1 : 0;
 			}
 		});
+		
+		Probe.regiterProbeTester(new ProbeFunction() {
+			@Override
+			public String getName() { return "FAT"; }
+
+			@Override
+			public Reader getReader() { return new FatReader(); }
+			
+			@Override
+			public float probe(byte[] sampleData) throws Throwable {
+				return FatReader.isFatFormated(sampleData) ? 0.99F : 0;
+			}
+		});
+		
+		Probe.regiterProbeTester(new ProbeFunction() {
+			@Override
+			public String getName() { return "___EMPTY___"; }
+
+			@Override
+			public Reader getReader() { return null; }
+			
+			@Override
+			public float probe(byte[] sampleData) throws Throwable {
+				return ByteArrayUtils.isEmptySector(sampleData) ? 1 : 0;
+			}
+		});
+		
 	}
 	
 	public static void main(String[] args) {
-		//testNTFS();
-		//System.out.println(SelectDriveGui.toHumanReadableFileSize(1072693248));
 		ci = new ConsoleInterface();
 		Screen screen = new Screen(1, 1, ci);
-		DriveInfo drive = new SelectDriveGui(screen, ci).awaitSelection();
 		
 		try {
-			ReadableSource source = drive.openReader();
+			ReadableSource source = new SelectDriveGui(screen, ci).awaitSelection();
 			PartitionReader r = new PartitionReader(source);
-			for(Partition p : r.getPartitions()){
+			ArrayList<Partition> partitions = r.getPartitions();
+			for(Partition p : partitions){
 				System.out.println(p);
+			}
+			//ImageFileWriter.write(partitions.get(0).source, new File("D:\\temp\\partition0.img"));
+			//ImageFileWriter.write(partitions.get(2).source, new File("D:\\temp\\partition2.img"));
+			//ZipFileWriter.write(source, diskImageZip, (1 << 20) / 512);
+			//ImageFileWriter.write(source, diskImageFile);
+			
+			/*
+			for(Partition p : r.getPartitions()){
+			//{Partition p = r.getPartitions().get(1);
 				byte[] buffer = new byte[512];
 				p.source.readSector(0, buffer);
 				ProbeResult result = Probe.detectType(buffer);
 				System.out.println(result);
+				
+				Reader reader = result.getSortedResults().get(0).getReader();
+				if(reader != null) reader.read(p.source);
+				/ *
+				if(reader instanceof NtfsReader){
+					NtfsReader ntfs = (NtfsReader)reader;
+					ntfs.read(p.source);
+					for(String file : ntfs.fileMap.keySet()){
+						//defender control
+						if(file != null && file.equalsIgnoreCase("defender")){
+							System.out.println(file);
+						}
+					}
+				}
+				* /
+				
 			}
+			*/
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	@SuppressWarnings("unused")
-	private static void testNTFS(){
-		int driveIndex = 1;
-		int partitionIndex = 1;
-		
-		try{
-			DriveInfo drive = WindowsDrives.listDrives().get(driveIndex);
-			ReadableSource source = drive.openReader();
-			PartitionReader r = new PartitionReader(source);
-			for(Partition p : r.getPartitions()){
-				System.out.println(p);
-			}
-			NtfsReader ntfs = new NtfsReader();
-			ntfs.read(r.getPartitions().get(partitionIndex).source);
-			/*
-			for(String file : ntfs.fileMap.keySet()){
-				if(file != null && file.endsWith(".txt")){
-					System.out.println(file);
-				}
-			}*/
-			NtfsNode node = ntfs.fileMap.get("Local\\Local\\Atlassian\\SourceTree\\git_local\\mingw32\\share\\doc\\connect\\manual.txt");
-			InputStream is = node.openInputStream();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			byte[] a = new byte[1024];
-			int l;
-			while((l = is.read(a)) != -1){
-				baos.write(a, 0, l);
-			}
-			System.out.println(new String(baos.toByteArray()));
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-
 }
