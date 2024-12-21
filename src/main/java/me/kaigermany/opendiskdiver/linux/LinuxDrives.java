103a974fffca00e4a64e0b0b5c0d3cf19ff074a5
kaigermany <48881973+kaigermany@users.noreply.github.com>
2024-12-21 21:26:09 +0100

    implemented Linux drive listing

--- src/main/java/me/kaigermany/opendiskdiver/linux/LinuxDrives.java
+++ src/main/java/me/kaigermany/opendiskdiver/linux/LinuxDrives.java
@@ -1,4 +1,4 @@
-package me.kaigermany.opendiskdiver.windows;
+package me.kaigermany.opendiskdiver.linux;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -6,24 +6,27 @@ import java.util.ArrayList;
 
 import me.kaigermany.opendiskdiver.data.DriveInfo;
 
-public class WindowsDrives {
+public class LinuxDrives {
 	public static ArrayList<DriveInfo> listDrives() throws UnsupportedOperationException {
 		try {
+			//n: no head, b: raw byte values, o:order filter, --raw:unformated output format.
 			Process p = new ProcessBuilder(
-					"powershell", "-Command", 
-					"Get-WmiObject Win32_DiskDrive | ForEach-Object { \\\"$($_.Caption)`n$($_.DeviceID)`n$($_.Size)\\\" }"
+					"lsblk", "-b", "--raw", "-o", "NAME,SIZE,TYPE", "-n"
 			).start();
-			
 			InputStream is = p.getInputStream();
 			ByteArrayOutputStream baos = new ByteArrayOutputStream(4096);
 			byte[] buf = new byte[1024];
 			int l;
 			while ((l = is.read(buf)) != -1) baos.write(buf, 0, l);
 			String data = new String(baos.toByteArray());
-			String[] a = data.split("\n");
-			ArrayList<DriveInfo> out = new ArrayList<DriveInfo>(a.length / 3);
-			for (int i = 0; i < a.length; i += 3) {
-				out.add(new DriveInfo(a[i], a[i + 1], a[i + 2].trim()));
+			ArrayList<DriveInfo> out = new ArrayList<DriveInfo>();
+			for(String entry : data.split("\n")){
+				String[] params = entry.split(" ");
+				if(!params[2].equals("disk")){
+					continue;
+				}
+				//out.add(new DriveInfo(params[0], "/sys/block/" + params[0], params[1]));
+				out.add(new DriveInfo(params[0], "/dev/" + params[0], params[1]));
 			}
 			return out;
 		} catch (Exception e) {
