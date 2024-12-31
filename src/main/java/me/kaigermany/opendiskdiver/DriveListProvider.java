6315a00f19d5a81b0950d53022a4ca5af74e8eeb
kaigermany <48881973+kaigermany@users.noreply.github.com>
2024-12-31 23:02:31 +0100

    hotfix: now reads correct sector sizes

--- src/main/java/me/kaigermany/opendiskdiver/DriveListProvider.java
+++ src/main/java/me/kaigermany/opendiskdiver/DriveListProvider.java
@@ -1,8 +1,10 @@
 package me.kaigermany.opendiskdiver;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import me.kaigermany.opendiskdiver.data.DriveInfo;
 import me.kaigermany.opendiskdiver.utils.Platform;
@@ -15,17 +17,9 @@ public class DriveListProvider {
 	private static ArrayList<DriveInfo> listDrivesLinux() throws UnsupportedOperationException {
 		try {
 			//n: no head, b: raw byte values, o:order filter, --raw:unformated output format.
-			Process p = new ProcessBuilder(
-					"lsblk", "-b", "--raw", "-o", "NAME,SIZE,TYPE", "-n"
-			).start();
-			InputStream is = p.getInputStream();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream(4096);
-			byte[] buf = new byte[1024];
-			int l;
-			while ((l = is.read(buf)) != -1) baos.write(buf, 0, l);
-			String data = new String(baos.toByteArray());
+			String[] lines = processResultLines("lsblk", "-b", "--raw", "-o", "NAME,SIZE,TYPE", "-n");
 			ArrayList<DriveInfo> out = new ArrayList<DriveInfo>();
-			for(String entry : data.split("\n")){
+			for(String entry : lines){
 				String[] params = entry.split(" ");
 				if(!params[2].equals("disk")){
 					continue;
@@ -41,25 +35,44 @@ public class DriveListProvider {
 	
 	private static ArrayList<DriveInfo> listDrivesWindows() throws UnsupportedOperationException {
 		try {
-			Process p = new ProcessBuilder(
-					"powershell", "-Command", 
-					"Get-WmiObject Win32_DiskDrive | ForEach-Object { \\\"$($_.Caption)`n$($_.DeviceID)`n$($_.Size)\\\" }"
-			).start();
+			String[] a = processResultLines("powershell", "-Command", 
+					"Get-WmiObject Win32_DiskDrive | ForEach-Object { \\\"$($_.Caption)`n$($_.DeviceID)`n$($_.Size)\\\" }");
 			
-			InputStream is = p.getInputStream();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream(4096);
-			byte[] buf = new byte[1024];
-			int l;
-			while ((l = is.read(buf)) != -1) baos.write(buf, 0, l);
-			String data = new String(baos.toByteArray());
-			String[] a = data.split("\n");
 			ArrayList<DriveInfo> out = new ArrayList<DriveInfo>(a.length / 3);
 			for (int i = 0; i < a.length; i += 3) {
 				out.add(new DriveInfo(a[i], a[i + 1], a[i + 2].trim()));
+			}
+			
+			//find correct sizes:
+			a = processResultLines("powershell", "-Command", 
+					"Get-Disk | Select-Object Number, Size | ForEach-Object { \\\"$($_.Number)`n$($_.Size)\\\" }");
+			HashMap<String, DriveInfo> driveMap = new HashMap<>(out.size() * 2);
+			for(DriveInfo di : out){
+				driveMap.put(di.path, di);
+			}
+			for (int i = 0; i < a.length; i += 2) {
+				String path = "\\\\.\\PHYSICALDRIVE" + a[i];
+				long exactSize = Long.parseLong(a[i+1].trim());
+				DriveInfo di = driveMap.get(path);
+				if(di.size < exactSize){
+					DriveInfo updatedInfo = new DriveInfo(di.name, path, exactSize);
+					out.set(out.indexOf(di), updatedInfo);
+				}
 			}
 			return out;
 		} catch (Exception e) {
 			throw new UnsupportedOperationException("Unable to execute PowerShell command", e);
 		}
+	}
+	
+	private static String[] processResultLines(String... programStartArguments) throws IOException {
+		Process p = new ProcessBuilder(programStartArguments).start();
+		InputStream is = p.getInputStream();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(4096);
+		byte[] buf = new byte[1024];
+		int l;
+		while ((l = is.read(buf)) != -1) baos.write(buf, 0, l);
+		String data = new String(baos.toByteArray());
+		return data.split("\n");
 	}
 }
