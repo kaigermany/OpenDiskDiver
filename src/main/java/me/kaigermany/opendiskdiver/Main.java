96d545fa2d6c7be05c62fc001c6629ae2abe6f03
kaigermany <48881973+kaigermany@users.noreply.github.com>
2025-11-23 14:48:02 +0100

    extracted the copySource()-Function

--- src/main/java/me/kaigermany/opendiskdiver/Main.java
+++ src/main/java/me/kaigermany/opendiskdiver/Main.java
@@ -1,36 +1,36 @@
 package me.kaigermany.opendiskdiver;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 import me.kaigermany.opendiskdiver.analysis.FatEntryScanner;
 import me.kaigermany.opendiskdiver.analysis.Scanner;
 import me.kaigermany.opendiskdiver.data.Reader;
-import me.kaigermany.opendiskdiver.data.fat.FatReader;
-import me.kaigermany.opendiskdiver.data.ntfs.NtfsReader;
 import me.kaigermany.opendiskdiver.data.partition.Partition;
 import me.kaigermany.opendiskdiver.data.partition.PartitionReader;
 import me.kaigermany.opendiskdiver.datafilesystem.FileSystem;
-import me.kaigermany.opendiskdiver.gui.DiskCopyState;
+import me.kaigermany.opendiskdiver.functions.CopyFunction;
 import me.kaigermany.opendiskdiver.gui.FileSystemBrowser;
 import me.kaigermany.opendiskdiver.gui.UI;
 import me.kaigermany.opendiskdiver.gui.UniversalUI;
 import me.kaigermany.opendiskdiver.gui.WindowsUI;
 import me.kaigermany.opendiskdiver.probe.Probe;
-import me.kaigermany.opendiskdiver.probe.ProbeFunction;
 import me.kaigermany.opendiskdiver.probe.ProbeResult;
 import me.kaigermany.opendiskdiver.reader.ReadableSource;
 import me.kaigermany.opendiskdiver.utils.ByteArrayUtils;
 import me.kaigermany.opendiskdiver.utils.DumpUtils;
 import me.kaigermany.opendiskdiver.utils.Platform;
 import me.kaigermany.opendiskdiver.utils.Utils;
-import me.kaigermany.opendiskdiver.writer.ImageFileWriter;
-import me.kaigermany.opendiskdiver.writer.Writer;
-import me.kaigermany.opendiskdiver.writer.ZipFileWriter;
 
 // https://www.grc.com/srrecovery.htm
 // http://web.archive.org/web/20190311160549/http://www.forensicswiki.org/wiki/Carving
@@ -41,27 +41,29 @@ public class Main {
 		final UI ui = createUI(false);
 		while(true){
 			int id = ui.cooseFromList("Welcome! Please coose your operation mode:", new String[]{
-					"Anaylzer Mode",	//call classic parsers.
-					"Copy Mode",		//allow disk copy tasks.
-					"Recovery Mode",	//specialized algorithms that try to recover as much as possible out of the given data.
+					"Anaylze a drive",
+					"Compare Drives or Images",
 					"Exit"
 			});
 			try{
-				ReadableSource source;
 				switch (id) {
 					case 0:
-						source = ui.cooseSource();
-						analyzeSource(source, ui);
+						driveOptions(ui, ui.cooseSource());
 						continue;
 					case 1:
-						source = ui.cooseSource();
-						copySource(source, ui);
+						{
+							ReadableSource source1 = ui.cooseSource();
+							ReadableSource source2 = ui.cooseSource();
+							ui.showInfo(new String[]{
+									"Select an output file for the log-dump."
+							}, false);
+							File logFile = ui.saveAs();
+							if(logFile != null){
+								compareDrives(ui, source1, source2, logFile);
+							}
+						}
 						continue;
 					case 2:
-						source = ui.cooseSource();
-						//TODO
-						continue;
-					case 3:
 						ui.close();
 						return;
 				}
@@ -80,115 +82,30 @@ public class Main {
 		}
 	}
 	
-	private static void copySource(ReadableSource source, UI ui) throws IOException {
-		int type = ui.cooseFromList("Please select an output file type:", new String[]{
-				".img - raw disk image",
-				".zip - zip file with blocks of compressed sectors",
-				"Abort"
-		});
-		if(type == 2) return;
-		
-		File outFile = null;
+	private static void driveOptions(UI ui, ReadableSource source) {
 		while(true){
-			outFile = ui.saveAs();
-			if(outFile.exists()){
-				int answer = ui.cooseFromList("File already exists!", new String[]{
-						"Rename it",
-						"continue and override it",
-						"Abort"
-				});
-				if(answer == 0) continue;
-				if(answer == 2) outFile = null;
-				break;
-			} else {
-				break;
-			}
-		}
-		if(outFile == null) return;
-		
-		long freeBytesOnTargetDisk = getFreeSpaceOnDisk(outFile);//.getFreeSpace();//outFile.getUsableSpace();
-		long bytesNeeded = source.numSectors() * 512;
-		if(freeBytesOnTargetDisk < bytesNeeded){
-			String title = "Warning: not enough space: expected: " 
-					+ Utils.toHumanReadableFileSize(bytesNeeded) + ", avaliable: "
-					+ Utils.toHumanReadableFileSize(freeBytesOnTargetDisk) + " -> missing: "
-					+ Utils.toHumanReadableFileSize(bytesNeeded - freeBytesOnTargetDisk) + " - CONTINUE?";
-			int answer = ui.cooseFromList(title, new String[]{
-					"No",
-					"Yes"
+			int id = ui.cooseFromList("Please coose your operation mode:", new String[]{
+					"Anaylzer Mode",	//call classic parsers.
+					"Copy drive",		//allow disk copy tasks.
+					"Exit"
 			});
-			if(answer == 0) return;
-		}
-		Writer writer = null;
-		switch(type){
-			case 0: {
-				writer = new ImageFileWriter();
-				break;
-			}
-			case 1: {
-				writer = new ZipFileWriter();
-				break;
-			}
-		}
-		writer.create(outFile, source);
-		try {
-			copyDisk(source, writer, ui);
-		} finally {
-			writer.close();
-		}
-	}
-	
-	public static void copyDisk(ReadableSource reader, Writer writer, UI ui) throws IOException {
-		DiskCopyState state = new DiskCopyState(reader.numSectors());
-		try{
-			byte[] buf = new byte[1 << 20];
-			int maxLen = buf.length / 512;
-			long pos = 0;
-			long maxPos = reader.numSectors();
-			while(pos < maxPos){
-				int numSectorsToRead = (int)Math.min(maxPos - pos, maxLen);
-				try{
-					
-					reader.readSectors(pos, numSectorsToRead, buf, 0);
-					writer.write(buf, numSectorsToRead * 512);
-					pos += numSectorsToRead;
-
-					state.setCurrentSector(pos);
-					ui.onDiskCopyStateUpdate(state);
-				}catch(IOException blockReadError){
-					//block read failed, try single sector read mode
-					byte[] dummyBuffer = new byte[512];
-					byte[] readBuffer = new byte[512];
-					for(int offset=0; offset<numSectorsToRead; offset++){
-						state.setCurrentSector(pos + offset);
-						ui.onDiskCopyStateUpdate(state);
-						try{
-							reader.readSectors(pos + offset, 1, readBuffer, 0);
-							writer.write(readBuffer, 512);
-						}catch(IOException sectorReadError){
-							state.incrUnreadableSectorCount();
-							writer.write(dummyBuffer, 512);
-						}
-					}
-					pos += numSectorsToRead;
+			try{
+				switch (id) {
+					case 0:
+						analyzeSource(source, ui);
+						continue;
+					case 1:
+						CopyFunction.copySource(source, ui);
+						continue;
+					case 2:
+						return;
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} finally {
-			ui.showInfo(new String[]{
-					"finished writing " + reader.numSectors() + " sectors.",
-					state.getUnreadableSectorCount() == 0 ? "No invalid sectors detected." : ("Found " + state.getUnreadableSectorCount() + " invalid sectors!")
-			}, true);
 		}
 	}
 	
-	private static long getFreeSpaceOnDisk(File fileOnDisk){
-		File root = fileOnDisk;
-		while(fileOnDisk != null){
-			root = fileOnDisk;
-			fileOnDisk = fileOnDisk.getParentFile();
-		}
-		return root.getUsableSpace();
-	}
 	
 	private static ReadableSource selectPartition(ReadableSource diskSource, UI ui) throws IOException {
 		ArrayList<Partition> partitions = new PartitionReader(diskSource).getPartitions();
@@ -369,26 +286,34 @@ public class Main {
 		}, false);
 		
 		final ArrayList<String> log = new ArrayList<>(1024);
+		final boolean[] hasMatchPtr = {false};
 		BiConsumer<Scanner, String> logCallback = new BiConsumer<Scanner, String>() {
 			@Override
 			public void accept(Scanner scanner, String msg) {
-				msg = "#" + (log.size() + 1) + " [" + scanner.getClass().getTypeName() + "] " + msg;
+				msg = "#" + (log.size() + 1) + " [" + scanner.getClass().getSimpleName() + "] " + msg;
 				log.add(msg);
+				hasMatchPtr[0] = true;
 			}
 		};
 		ArrayList<Scanner> scannerList = new ArrayList<>();
 		scannerList.add(new FatEntryScanner());
+		scannerList.add(new FileHeaderScanner());
+		
 		
 		long diskSize = source.numSectors();
 		byte[] buffer = new byte[512];
 		long lastRedraw = System.currentTimeMillis();
+		ArrayList<Long> matchOffsets = new ArrayList<>();
 		for(long offset=0; offset<diskSize; offset++){
 			source.readSector(offset, buffer);
 			
 			for(Scanner scanner : scannerList){
 				scanner.scan(buffer, offset, logCallback);
 			}
-			
+			if(hasMatchPtr[0]){
+				hasMatchPtr[0] = false;
+				matchOffsets.add(offset);
+			}
 			long t = System.currentTimeMillis();
 			if(t - 250 >= lastRedraw){
 				lastRedraw = t;
@@ -399,6 +324,239 @@ public class Main {
 				ui.showInfo(consoleMessage, false);
 			}
 		}
+		
+		long[] clusterSizeAndOffset = guessClusterSizeAndOffset(matchOffsets);
+		System.out.println("clusterSize = " + clusterSizeAndOffset[0]);
+		System.out.println("Offset = " + clusterSizeAndOffset[1]);
+		
+		//restart with the new information:
+		log.clear();
+		long stepSize = clusterSizeAndOffset[0];
+		for(long offset=clusterSizeAndOffset[1]; offset<diskSize; offset+=stepSize){
+			source.readSector(offset, buffer);
+			
+			for(Scanner scanner : scannerList){
+				scanner.scan(buffer, offset, logCallback);
+			}
+			if(hasMatchPtr[0]){
+				hasMatchPtr[0] = false;
+				matchOffsets.add(offset);
+			}
+			long t = System.currentTimeMillis();
+			if(t - 250 >= lastRedraw){
+				lastRedraw = t;
+				int maxSize = Math.min(10, log.size());
+				String[] consoleMessage = new String[maxSize + 1];
+				log.subList(log.size() - maxSize, log.size()).toArray(consoleMessage);
+				consoleMessage[maxSize] = "Scanner Location: " + offset + " / " + diskSize + "   " + (Math.floor(10000D * offset / diskSize) / 100) + " %";
+				ui.showInfo(consoleMessage, false);
+			}
+		}
+		
 		ui.showInfo(log.toArray(new String[log.size()]), true);
+	}
+
+	private static long[] guessClusterSizeAndOffset(ArrayList<Long> matchOffsets) {
+		//a x a to match every hit against every other:
+		HashMap<Long, long[]> ggtResultsScored = new HashMap<>();
+		for(int p1=0; p1<matchOffsets.size()-1; p1++){
+			for(int p2=p1+1; p2<matchOffsets.size(); p2++){
+				long pos1 = matchOffsets.get(p1);
+				long pos2 = matchOffsets.get(p2);
+				long div = ggt(pos1, pos2);
+				ggtResultsScored.computeIfAbsent(div, (k) -> new long[]{0, pos1})[0]++;
+			}
+		}
+		Map.Entry<Long, long[]> best = null;
+		for(Map.Entry<Long, long[]> e : ggtResultsScored.entrySet()){
+			if(best == null || e.getValue()[0] > best.getValue()[0]){
+				best = e;
+			}
+		}
+		
+		if(best == null) return null;
+		return new long[]{best.getKey(), best.getValue()[1]};
+	}
+	
+	private static long ggt(long a, long b){
+		long c = a - b;
+		if(a == 0 || b == 0 || c == 0) {
+			return 0;
+		}
+		do{
+			c = a;
+			a = b;
+			b = c % a;
+		}while(b != 0);
+		return a;
+	}
+	
+
+	
+	private static void compareDrives(UI ui, ReadableSource source1, ReadableSource source2, File logFile) {
+		try{
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(logFile), 4096);
+			try {
+				long cnt1 = source1.numSectors();
+				long cnt2 = source2.numSectors();
+				bos.write((
+						"Source1.numSectors = " + cnt1 + "\r\n"
+					+	"Source2.numSectors = " + cnt2 + "\r\n"
+					).getBytes(StandardCharsets.UTF_8));
+				if (cnt1 != cnt2) {
+					bos.write((
+							"Source1.numSectors MUST be Source2.numSectors! -> exit."
+						).getBytes(StandardCharsets.UTF_8));
+					return;
+				}
+				compareDrivesDetailed(ui, source1, source2, bos);
+				//compareDrivesSimple(ui, source1, source2, bos);
+			} finally {
+				bos.close();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	private static void compareDrivesSimple(UI ui, ReadableSource source1, ReadableSource source2, BufferedOutputStream bos) throws IOException {
+		long size = source1.numSectors();
+		byte[] sector1 = new byte[512];
+		byte[] sector2 = sector1.clone();
+		long diffCount = 0;
+		long lastRedraw = 0;
+		long lastPos = 0;
+		boolean isEqualRegion = true;
+		for (long pos = 0; pos < size; pos++) {
+			long t = System.currentTimeMillis();
+			if (t - 250 >= lastRedraw) {
+				lastRedraw = t;
+				ui.showInfo(new String[] {
+					"Scanner Location: " + pos + " / " + size + "   " + (Math.floor(10000D * pos / size) / 100) + " %",
+					"diffCount: " + diffCount
+				}, false);
+			}
+			
+			source1.readSector(pos, sector1);
+			source2.readSector(pos, sector2);
+			
+			boolean isEqual = Arrays.equals(sector1, sector2);
+			if(isEqual != isEqualRegion){
+				long last = pos - 1;
+				String logMsg;
+				if(last < lastPos){
+					logMsg = "Single " + (isEqualRegion ? "Equal" : "Invalid") + " sector at " + lastPos + "\r\n";
+				} else {
+					logMsg = (isEqualRegion ? "Equal" : "Invalid") + " region from " + lastPos + " to " + last + "\r\n";
+				}
+				bos.write(logMsg.getBytes(StandardCharsets.UTF_8));
+				isEqualRegion = isEqual;
+				lastPos = pos;
+			}
+			if(!isEqual){
+				diffCount++;
+			}
+		}
+		long last = size - 1;
+		String logMsg;
+		if(last < lastPos){
+			logMsg = "Single " + (isEqualRegion ? "Equal" : "Invalid") + " sector at " + lastPos + "\r\n";
+		} else {
+			logMsg = (isEqualRegion ? "Equal" : "Invalid") + " region from " + lastPos + " to " + last + "\r\n";
+		}
+		bos.write(logMsg.getBytes(StandardCharsets.UTF_8));
+	}
+	
+	private static void compareDrivesDetailed(UI ui, ReadableSource source1, ReadableSource source2, BufferedOutputStream bos) throws IOException {
+		long size = source1.numSectors();
+		byte[] sector1 = new byte[512];
+		byte[] sector2 = sector1.clone();
+		byte[] xorDiff = sector1.clone();
+		long diffCount = 0;
+		long lastRedraw = 0;
+		for (long pos = 0; pos < size; pos++) {
+			long t = System.currentTimeMillis();
+			if (t - 250 >= lastRedraw) {
+				lastRedraw = t;
+				ui.showInfo(new String[] {
+					"Scanner Location: " + pos + " / " + size + "   " + (Math.floor(10000D * pos / size) / 100) + " %",
+					"diffCount: " + diffCount
+				}, false);
+			}
+			
+			source1.readSector(pos, sector1);
+			source2.readSector(pos, sector2);
+			for(int i=0; i<512; i++) xorDiff[i] = (byte)(sector1[i] ^ sector2[i]);
+			if (ByteArrayUtils.isEmptySector(xorDiff)) {
+				continue;
+			} else {
+				int numDifferingBytes = 0;
+				for(int i=0; i<512; i++) if(xorDiff[i] != 0) numDifferingBytes++;
+				boolean areOnlyBitflips = true;
+				for(int i=0; i<512; i++) {
+					if(Integer.bitCount(xorDiff[i] & 0xFF) > 1) {
+						areOnlyBitflips = false;
+						break;
+					}
+				}
+				
+				if(numDifferingBytes > 4 && !areOnlyBitflips){
+					String d1 = DumpUtils.binaryDumpToString(sector1);
+					String d2 = DumpUtils.binaryDumpToString(sector2);
+					bos.write((
+						"Diff at sector " + pos + ": numDifferingBytes = " + numDifferingBytes + "\r\n"
+						+ "----- Source 1: -----\r\n"
+						+ d1
+						+"\r\n"
+						+ "----- Source 2: -----\r\n"
+						+ d2
+						+"\r\n"
+					).getBytes(StandardCharsets.UTF_8));
+				} else {
+					StringBuilder sb = new StringBuilder(1024);
+					if(areOnlyBitflips){
+						sb.append("Diff at sector ").append(pos)
+							.append(": numDifferingBytes = ").append(numDifferingBytes)
+							.append("\r\n  FROM   |    TO    |   XOR    | pos in sector");
+						for(int i=0; i<512; i++){
+							if(xorDiff[i] != 0){
+								int b1 = 256 | (sector1[i] & 0xFF);
+								int b2 = 256 | (sector2[i] & 0xFF);
+								sb.append("\r\n")
+								.append(Integer.toBinaryString(b1).substring(1))
+								.append(" | ")
+								.append(Integer.toBinaryString(b2).substring(1))
+								.append(" | ")
+								.append(Integer.toBinaryString(256 | (xorDiff[i] & 0xFF)).substring(1))
+								.append(" | ").append(i);
+							}
+						}
+					} else {
+						sb.append("Diff at sector ").append(pos)
+							.append(": numDifferingBytes = ").append(numDifferingBytes)
+							.append("\r\n  FROM   |    TO    |   XOR    | from |  to  | pos in sector");
+						for(int i=0; i<512; i++){
+							if(xorDiff[i] != 0){
+								int b1 = 256 | (sector1[i] & 0xFF);
+								int b2 = 256 | (sector2[i] & 0xFF);
+								sb.append("\r\n")
+								.append(Integer.toBinaryString(b1).substring(1))
+								.append(" | ")
+								.append(Integer.toBinaryString(b2).substring(1))
+								.append(" | ")
+								.append(Integer.toBinaryString(256 | (xorDiff[i] & 0xFF)).substring(1))
+								.append(" | 0x")
+								.append(Integer.toHexString(b1).substring(1).toUpperCase())
+								.append(" | 0x")
+								.append(Integer.toHexString(b2).substring(1).toUpperCase())
+								.append(" | ").append(i);
+							}
+						}
+					}
+					bos.write(sb.append("\r\n\r\n").toString().getBytes(StandardCharsets.UTF_8));
+				}
+				diffCount++;
+			}
+		}
 	}
 }
---
+++ src/main/java/me/kaigermany/opendiskdiver/functions/CopyFunction.java
@@ -0,0 +1,125 @@
+package me.kaigermany.opendiskdiver.functions;
+
+import java.io.File;
+import java.io.IOException;
+
+import me.kaigermany.opendiskdiver.gui.DiskCopyState;
+import me.kaigermany.opendiskdiver.gui.UI;
+import me.kaigermany.opendiskdiver.reader.ReadableSource;
+import me.kaigermany.opendiskdiver.utils.Utils;
+import me.kaigermany.opendiskdiver.writer.ImageFileWriter;
+import me.kaigermany.opendiskdiver.writer.Writer;
+import me.kaigermany.opendiskdiver.writer.ZipFileWriter;
+
+public class CopyFunction {
+	public static void copySource(ReadableSource source, UI ui) throws IOException {
+		int type = ui.cooseFromList("Please select an output file type:", new String[]{
+				".img - raw disk image",
+				".zip - zip file with blocks of compressed sectors",
+				"Abort"
+		});
+		Writer writer = null;
+		switch(type){
+			case 0: {
+				writer = new ImageFileWriter();
+				break;
+			}
+			case 1: {
+				writer = new ZipFileWriter();
+				break;
+			}
+			case 2: return;
+		}
+		
+		File outFile = null;
+		while(true){
+			outFile = ui.saveAs();
+			if(outFile.exists()){
+				int answer = ui.cooseFromList("File already exists!", new String[]{
+						"Rename it",
+						"continue and override it",
+						"Abort"
+				});
+				if(answer == 0) continue;
+				if(answer == 2) outFile = null;
+				break;
+			} else {
+				break;
+			}
+		}
+		if(outFile == null) return;
+		
+		long freeBytesOnTargetDisk = getFreeSpaceOnDisk(outFile);
+		long bytesNeeded = source.numSectors() * 512;
+		if(freeBytesOnTargetDisk < bytesNeeded){
+			String title = "Warning: not enough space: expected: " 
+					+ Utils.toHumanReadableFileSize(bytesNeeded) + ", avaliable: "
+					+ Utils.toHumanReadableFileSize(freeBytesOnTargetDisk) + " -> missing: "
+					+ Utils.toHumanReadableFileSize(bytesNeeded - freeBytesOnTargetDisk) + " - CONTINUE?";
+			int answer = ui.cooseFromList(title, new String[]{
+					"No",
+					"Yes"
+			});
+			if(answer == 0) return;
+		}
+		
+		writer.create(outFile, source);
+		try {
+			copyDisk(source, writer, ui);
+		} finally {
+			writer.close();
+		}
+	}
+	
+	public static void copyDisk(ReadableSource reader, Writer writer, UI ui) throws IOException {
+		DiskCopyState state = new DiskCopyState(reader.numSectors());
+		try{
+			byte[] buf = new byte[1 << 20];
+			int maxLen = buf.length / 512;
+			long pos = 0;
+			long maxPos = reader.numSectors();
+			while(pos < maxPos){
+				int numSectorsToRead = (int)Math.min(maxPos - pos, maxLen);
+				try{
+					
+					reader.readSectors(pos, numSectorsToRead, buf, 0);
+					writer.write(buf, numSectorsToRead * 512);
+					pos += numSectorsToRead;
+
+					state.setCurrentSector(pos);
+					ui.onDiskCopyStateUpdate(state);
+				}catch(IOException blockReadError){
+					//block read failed, try single sector read mode
+					byte[] dummyBuffer = new byte[512];
+					byte[] readBuffer = new byte[512];
+					for(int offset=0; offset<numSectorsToRead; offset++){
+						state.setCurrentSector(pos + offset);
+						ui.onDiskCopyStateUpdate(state);
+						try{
+							reader.readSectors(pos + offset, 1, readBuffer, 0);
+							writer.write(readBuffer, 512);
+						}catch(IOException sectorReadError){
+							state.incrUnreadableSectorCount();
+							writer.write(dummyBuffer, 512);
+						}
+					}
+					pos += numSectorsToRead;
+				}
+			}
+		} finally {
+			ui.showInfo(new String[]{
+					"finished writing " + reader.numSectors() + " sectors.",
+					state.getUnreadableSectorCount() == 0 ? "No invalid sectors detected." : ("Found " + state.getUnreadableSectorCount() + " invalid sectors!")
+			}, true);
+		}
+	}
+	
+	private static long getFreeSpaceOnDisk(File fileOnDisk){
+		File root = fileOnDisk;
+		while(fileOnDisk != null){
+			root = fileOnDisk;
+			fileOnDisk = fileOnDisk.getParentFile();
+		}
+		return root.getUsableSpace();
+	}
+}
